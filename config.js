4e209522231b1a5e85869e0d066d05442cf81c2b
medioloper <115008048+DKULena@users.noreply.github.com>
2024-06-19 21:24:05 +0900

    Update config.js
    rename blog title, position

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const siteConfig = {
   repositoryName: "", // GitHub 저장소 이름
   mainColor: "#3498db", // 사이트의 주 색상
   textColor: "#333333", // 기본 텍스트 색상
-  blogTitle: "Medioloper의 개발 일기", // 블로그 제목
+  blogTitle: "98년생 Medioloper 개발일기", // 블로그 제목
 };
 
 // 여러명의 저자가 글을 쓸 경우 프로필 설정, default는 0번째 사용자
@@ -15,7 +15,7 @@ const users = [
     id: 0, // default author
     username: "Lena",
     company: "EST Bootcamp",
-    position: "Student",
+    position: "Tofu",
     img: "img/user/div100.png",
   },
 ];
